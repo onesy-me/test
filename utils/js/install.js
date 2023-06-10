c72ba36950f4c6744bfbe83e317ce489d979039c
Lazar Eric <lazareric.com@gmail.com>
2023-06-10 11:25:38 +0200

    m

--- utils/js/install.js
+++ utils/js/install.js
@@ -1,5 +1,5 @@
-const fse = require('fs-extra');
 const path = require('path');
+const fse = require('fs-extra');
 
 // Add amaui-test entire build folder to @types so global types are automatically used by Typescript
 fse.copy(process.cwd(), path.resolve('../../@types/amaui-test'));
